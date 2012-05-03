09deacb770640e3a42d1ba3b4deaf3a08e699544
Kristian Sons <ksons@cg.uni-saarland.de>
2012-05-03 13:49:44 +0200

    MotionFactory and Moveable

--- src/interfaces.js
+++ src/interfaces.js
@@ -4,7 +4,19 @@ var XMOT = XMOT || {};
 /** @define {string} */
 XMOT.version = '%VERSION%';
 
-
 (function() {
+
+    /**
+     * A MotionFactory.
+     * @interface
+     */
+    function MotionFactory() {};
+
+    /**
+     * A Moveable.
+     * @interface
+     */
+    function Moveable() {};
+    Moveable.prototype.draw = function() {};
 
 }());
