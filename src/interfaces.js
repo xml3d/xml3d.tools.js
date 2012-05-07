eba938606bfc79969fee812a4b6aff8f5a23cea0
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-07 14:06:30 +0200

    Extended the interfaces MotionFactory and Moveable

--- src/interfaces.js
+++ src/interfaces.js
@@ -10,13 +10,28 @@ XMOT.version = '%VERSION%';
      * A MotionFactory.
      * @interface
      */
-    function MotionFactory() {};
+    var MotionFactory = function() {
+    };
+    var m = MotionFactory.prototype;
+
+    //functions
+    m.createMoveable = function(){};
 
     /**
      * A Moveable.
      * @interface
      */
-    function Moveable() {};
-    Moveable.prototype.draw = function() {};
+    var Moveable = function() {
+    };
+    var p = Moveable.prototype;
 
+    //functions
+    p.setPosition = function(){};
+    p.setOrientation = function(){};
+    p.translate = function(){};
+    p.rotate = function(){};
+
+    //export to namespace
+    XMOT.MotionFactory = MotionFactory;
+    XMOT.Moveable = Moveable;
 }());
