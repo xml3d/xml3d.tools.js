e2ba92ec2e1be69e8e34d648e9ba4429e77f3cf4
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-07 16:47:25 +0200

    Added Testing methode for rotation

--- examples/simple.js
+++ examples/simple.js
@@ -1,9 +1,22 @@
+muh = 0;
+
 function rotateCube()
 {
+	document.getElementById("notification").innerHTML = "Rotating the Cube ...";
+	var cube = document.getElementById("cube");
+	var factory = new XMOT.ClientMotionFactory;
+	var moveable = factory.createMoveable(cube);
+	if (!moveable)
+		return;
+	else{
+		if(muh%2)
+			moveable.setOrientation(0.0, 1.0, 0.0, 0.5);
+		else
+			moveable.setOrientation(0.0, 1.0, 0.0, 0.25);
+	}
+	muh++;
 	document.getElementById("notification").innerHTML = "Rotated the Cube ...";
 }
-
-muh = 0;
 
 function moveCube()
 {
