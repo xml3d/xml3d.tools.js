cd14b481df072618605008331bbc1ef20e067418
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-09 18:28:20 +0200

    Adopted example to interface changes; instantiate only one moveable

--- examples/simple.js
+++ examples/simple.js
@@ -1,57 +1,41 @@
 muh = 0;
+moveable = undefined;
+function initMoveable(){
+	var cube = document.getElementById("cube");
+	var factory = new XMOT.ClientMotionFactory;
+	var constraint = new XMOT.CollisionConstraint(5.0, 5.0, [0,1,0], "collision.png");
+	moveable = factory.createMoveable(cube, constraint);
+};
 
 function rotateCube()
 {
 	document.getElementById("notification").innerHTML = "Rotating the Cube ...";
-	var cube = document.getElementById("cube");
-	var factory = new XMOT.ClientMotionFactory;
-	var moveable = factory.createMoveable(cube);
-	if (!moveable)
-		return;
-	else{
-		if(muh%2)
-			moveable.setOrientation(0.0, 1.0, 0.0, 0.5);
-		else
-			moveable.setOrientation(0.0, 1.0, 0.0, 0.25);
-	}
+	if(muh%2)
+		moveable.setOrientation(0.0, 1.0, 0.0, 0.5);
+	else
+		moveable.setOrientation(0.0, 1.0, 0.0, 0.25);
 	muh++;
 	document.getElementById("notification").innerHTML = "Rotated the Cube ...";
 }
 
 function moveCube()
 {
-	//TODO: error handling
 	document.getElementById("notification").innerHTML = "Moving the Cube ...";
-	var cube = document.getElementById("cube");
-	var factory = new XMOT.ClientMotionFactory;
-	var moveable = factory.createMoveable(cube);
-	if (!moveable)
-		return;
-	else{
-		if(muh%2)
-			moveable.setPosition(1.0, 0.0, 1.0);
-		else
-			moveable.setPosition(2.0, 0.0, 2.0);
-	}
+	if(muh%2)
+		moveable.setPosition(1.0, 0.0, 1.0);
+	else
+		moveable.setPosition(2.0, 0.0, 2.0);
 	muh++;
 	document.getElementById("notification").innerHTML = "Moved the Cube ...";
 }
 
 function translateCube()
 {
-	//TODO: error handling
 	document.getElementById("notification").innerHTML = "Translating the Cube ...";
-	var cube = document.getElementById("cube");
-	var factory = new XMOT.ClientMotionFactory;
-	var moveable = factory.createMoveable(cube);
-	if (!moveable)
-		return;
-	else{
-		if(muh%2)
-			moveable.translate(1.0, 0.0, 1.0);
-		else
-			moveable.translate(-2.0, 0.0, -2.0);
-	}
+	if(muh%2)
+		moveable.translate(1.0, 0.0, 1.0);
+	else
+		moveable.translate(-2.0, 0.0, -2.0);
 	muh++;
 	document.getElementById("notification").innerHTML = "Translated the Cube ...";
 }
