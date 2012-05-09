20df2341e2d06c6b799e8f26b44c86575a8a9a9f
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-09 18:25:08 +0200

    Added constraint support to the ClientMotionFactory

--- src/ClientMotionFactory.js
+++ src/ClientMotionFactory.js
@@ -9,14 +9,13 @@
 	};
 
 	var m = ClientMotionFactory.prototype;
-	m.createMoveable = function(obj){
-		//TODO: sync with server?
+	m.createMoveable = function(obj, constraint){
 		var t = XML3D.URIResolver.resolve(obj.transform, obj.ownerDocument);
 		if (!t) {
 			throw "Object does not have a transfrom property.";
 			return null;
 		}
-		return new XMOT.ClientMoveable(obj, t);
+		return new XMOT.ClientMoveable(obj, t, constraint);
 	};
 
 	//export
