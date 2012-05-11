dafda9117ae52caa9b5bcf6e7e4167692314788b
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-11 16:44:35 +0200

    ClientAnimatable - first Version

---
+++ src/ClientAnimatable.js
@@ -0,0 +1,35 @@
+(function(){
+
+	/**
+	 * An implementation of Animatable
+	 * @implements Animatable
+	 */
+	var Animatable = function(){
+		//stores available animations by their name
+		this.availableAnimations = {};
+		//stores active animations by their id, if they have an id
+		this.activeAnimations = {};
+	};
+    var a = Animatable.prototype;
+
+    /** @inheritDoc */
+    a.addAnimation = function(animation){
+		this.availableAnimations[animation.name] = animation;
+    };
+
+    /** @inheritDoc */
+    a.startAnimation = function(name, opt){
+		var toStart = this.availableAnimations[name];
+		toStart.start(opt);
+    };
+
+    /** @inheritDoc */
+    a.stopAnimation = function(id){
+		var toStop = activeAnimations[id];
+		if(toStop) toStop.stop();
+    };
+
+    //export
+	XMOT.Animatable = Animatable;
+
+}());
