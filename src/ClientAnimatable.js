8431f716eb8a666e9298810307eed2d9707b01fe
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-14 13:25:01 +0200

    Added forgotten returns

--- src/ClientAnimatable.js
+++ src/ClientAnimatable.js
@@ -24,12 +24,14 @@
     a.startAnimation = function(name, opt){
 		var toStart = this.availableAnimations[name];
 		toStart.start(opt);
+		return this;
     };
 
     /** @inheritDoc */
     a.stopAnimation = function(id){
 		var toStop = activeAnimations[id];
 		if(toStop) toStop.stop();
+		return this;
     };
 
     //export
