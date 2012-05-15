65a454aab63b027c78ed57f5318e4173f32b7106
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-15 16:10:36 +0200

    Added XML3D externs to make the closure compiler happy

---
+++ src/externs.js
@@ -0,0 +1,26 @@
+/**
+ * @externs
+ */
+
+var XML3D = {};
+
+XML3D.URIResolver = {};
+
+/**
+ * @param {Object} transform
+ * @param {Object} ownerDoc
+ */
+XML3D.URIResolver.resolve = function(transform, ownerDoc){};
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @constructor
+ */
+function XML3DVec3(x,y,z){};
+
+/**
+ * @constructor
+ */
+function XML3DRotation(){};
