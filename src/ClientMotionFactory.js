d14f2b48eff9978ac0693a8ca1b9117ae5826a3c
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-05-15 09:47:15 +0200

    Implemented Factory functions for animatable and animations

--- src/ClientMotionFactory.js
+++ src/ClientMotionFactory.js
@@ -20,6 +20,31 @@
 		return new XMOT.ClientMoveable(obj, t, constraint);
 	};
 
+	/** @inheritDoc */
+	m.createAnimatable = function(obj, constraint){
+		var t = XML3D.URIResolver.resolve(obj.transform, obj.ownerDocument);
+		if (!t) {
+			throw "Object does not have a transfrom property.";
+			return null;
+		}
+		return new XMOT.ClientAnimatable(obj, t, constraint);
+	};
+
+	/** @inheritDoc */
+    m.createKeyframeAnimation = function(name, type, element, opt){
+		//TODO: this works with WebGL only?
+		//TODO: error handling?
+		var child = element.firstElementChild;
+		var keys = child.value;
+		child = child.nextElementSibling;
+		var values = child.value;
+		if(!keys || !values){
+			throw "Object is not a valid keyframe animation";
+			return null;
+		}
+		return new XMOT.ClientKeyframeAnimation(name, type, keys, values, opt);
+    };
+
 	//export
 	XMOT.ClientMotionFactory = ClientMotionFactory;
 }());
