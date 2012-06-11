e5146f7c0ba4e6a5306a807b7955ecc0c0c9bcda
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-06-11 11:19:09 +0200

    Provide function axis angle to quaternion conversion

--- src/util.js
+++ src/util.js
@@ -23,9 +23,38 @@ function animate(){
 		XMOT.animating = false;
 };
 
+/**
+ * Converts axis angle representation into an quaternion
+ * @param {Array.<number>} axis
+ * @param {number} angle
+ * @return {Array.<number>} quaternion
+ */
+function axisAngleToQuaternion(axis, angle){
+	var normAxis = XMOT.normalizeVector(axis);
+	var quat = [];
+	var s = Math.sin(angle/2);
+	quat[0] = normAxis[0] *s;
+	quat[1] = normAxis[1] *s;
+	quat[2] = normAxis[2] *s;
+	quat[3] = Math.cos(angle/2);
+	return quat;
+}
+
+/**
+ * Normalizes a 3D vector
+ * @param {Array.<number>} vector
+ * @return {Array.<number>} normalized vector
+ */
+function normalizeVector(vector){
+	var length = Math.sqrt( vector[0]*vector[0] + vector[1]*vector[1] + vector[2]*vector[2] );
+	if(length == 0) return vector;
+	return [vector[0]/length, vector[1]/length, vector[2]/length];
+}
+
 //export
 XMOT.animate = animate;
 XMOT.animating = animating;
 XMOT.animationHook = animationHook;
-
+XMOT.axisAngleToQuaternion = axisAngleToQuaternion;
+XMOT.normalizeVector = normalizeVector;
 }());
