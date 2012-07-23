d83e88a9f6dd8d125123411a0761f01418e8c423
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-07-23 12:52:24 +0200

    Corrected and optimized simple example

--- examples/simple.js
+++ examples/simple.js
@@ -45,9 +45,11 @@ function moveCubeTo()
 {
 	document.getElementById("notification").innerHTML = "Moving the Cube To ...";
 	moveable.moveTo([1.0, 0.0, 1.0], XMOT.axisAngleToQuaternion([0.0, 1.0, 0.0], 0.5), 1000);
-	moveable.moveTo(undefined, XMOT.axisAngleToQuaternion([0.0, 1.0, 0.0], 1.5), 1500);
+	moveable.moveTo(undefined, XMOT.axisAngleToQuaternion([0.0, 1.0, 0.0], Math.PI), 1500);
 	moveable.moveTo([1.0, 0.0, 4.0], undefined, 2000);
-	moveable.moveTo([4.0, 0.0, 4.0], XMOT.axisAngleToQuaternion([1.0, 1.0, 0.0], 0.0), 2500);
+	moveable.moveTo([4.0, 0.0, 4.0], undefined, 2500);
+	moveable.moveTo([4.0, 0.0, 1.0], undefined, 3000);
+	moveable.moveTo([1.0, 0.0, 1.0], XMOT.axisAngleToQuaternion([0.0, 1.0, 0.0], 0.5), 3500);
 	document.getElementById("notification").innerHTML = "Ehm... Cube should have moved";
 };
 
