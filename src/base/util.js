be9657146bedbd3f35689d3ee3191e09cb8451ed
Johannes Ebersold <ebersold@cg.uni-saarland.de>
2012-10-25 09:34:12 +0200

    Removed some unnecessary semicolons (in order to fix ide warnings)

--- src/base/util.js
+++ src/base/util.js
@@ -26,12 +26,12 @@
  */
 function inherit(child, parent) {
 	/** @constructor */
-	function tmp() {};
+	function tmp() {}
 	tmp.prototype = parent.prototype;
 	child.superClass_ = parent.prototype;
 	child.prototype = new tmp();
 	child.prototype.constructor = child;
-};
+}
 
 /**
  * Call up to the superclass.
@@ -82,7 +82,7 @@ function base(me, opt_methodName, var_args) {
 	} else {
 		throw "base called from a method of one name to a method of a different name";
 	}
-};
+}
 
 // ----------------------------------------------------------------------------
 
@@ -113,7 +113,7 @@ function animate(){
 	}
 	else
 		XMOT.animating = false;
-};
+}
 
 /**
  * Converts axis angle representation into an quaternion
@@ -130,7 +130,7 @@ function axisAngleToQuaternion(axis, angle){
 	quat[2] = normAxis[2] *s;
 	quat[3] = Math.cos(angle/2);
 	return quat;
-};
+}
 
 /**
  * Normalizes a 3D vector
@@ -141,7 +141,7 @@ function normalizeVector(vector){
 	var length = Math.sqrt( vector[0]*vector[0] + vector[1]*vector[1] + vector[2]*vector[2] );
 	if(length == 0) return vector;
 	return [vector[0]/length, vector[1]/length, vector[2]/length];
-};
+}
 
 /**
  * Converts a quaternion into an axis angle representation
@@ -157,7 +157,7 @@ function quaternionToAxisAngle(quat){
 	var y = quat[1]/s;
 	var z = quat[2]/s;
 	return {axis:[x,y,z], angle:angle};
-};
+}
 
 /**
  * Interpolate between two quaternions the shortest way
@@ -199,7 +199,7 @@ function slerp(from, to, t) {
 	result[2] = p * from[2] + q * to[2];
 	result[3] = p * from[3] + q * to[3];
 	return result;
-};
+}
 
 /**
  * Merges two optionsobjects
@@ -217,7 +217,7 @@ function mergeOptions(high, low){
 	ret.easing 		= high.easing 	|| low.easing;
 	ret.callback 	= high.callback || low.callback;
 	return ret;
-};
+}
 
 //export
 XMOT.inherit = inherit;
