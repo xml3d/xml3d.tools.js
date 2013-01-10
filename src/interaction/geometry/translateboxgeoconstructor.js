31b05a71be57df407a9936feee0adff06a3148ef
Boris Broenner <bobbel@cg.uni-saarland.de>
2013-01-10 13:55:43 +0100

    translatebox: adapted to geo-constructor concept

---
+++ src/interaction/geometry/translateboxgeoconstructor.js
@@ -0,0 +1,66 @@
+XMOT.namespace("XMOT.interaction.geometry");
+
+XMOT.interaction.geometry.TranslateBoxGeoConstructor = new XMOT.Class({
+
+	initialize: function(widget)
+	{
+		this.geo = widget.geo;
+	},
+
+	createDefsElements: function()
+	{
+        // shaders
+        this.geo.addShaders("s_transl", {
+            diffCol: "1 1 1", transp: "0.85"
+        });
+        this.geo.addShaders("s_transl_highlight", {
+            diffCol: "1 1 1", transp: "0.4"
+        });
+
+        // transforms
+        this.geo.addTransforms("t_xytrans", {
+            translation: "0 0 1"});
+        this.geo.addTransforms("t_xytrans_inv", {
+            translation: "0 0 -1", rotation: "0 1 0 3.14"});
+        this.geo.addTransforms("t_yztrans", {
+            translation: "1 0 0", rotation: "0 1 0 1.57"});
+        this.geo.addTransforms("t_yztrans_inv", {
+            translation: "-1 0 0", rotation: "0 1 0 -1.57"});
+        this.geo.addTransforms("t_xztrans", {
+            translation: "0 1 0", rotation: "1 0 0 -1.57"});
+        this.geo.addTransforms("t_xztrans_inv", {
+            translation: "0 -1 0", rotation: "1 0 0 1.57"});
+	},
+
+	createGraph: function()
+	{
+        this.geo.addToGraphRoot([
+             this._createRectGrp("xytrans"),
+             this._createRectGrp("yztrans"),
+             this._createRectGrp("xztrans"),
+             this._createRectGrp("xytrans_inv"),
+             this._createRectGrp("yztrans_inv"),
+             this._createRectGrp("xztrans_inv")
+        ]);
+	},
+
+    /**
+     *  @this {XMOT.interaction.widgets.TranslateBox}
+     *  @private
+     *
+     *  @param {string} localID
+     */
+    _createRectGrp: function(localID)
+    {
+        var rect = XMOT.creation.rectangle(this.geo.xml3d);
+
+        var opts = {
+            id: this.geo.globalID(localID),
+            transform: "#" + this.geo.globalID("t_" + localID),
+            shader: "#" + this.geo.globalID("s_transl"),
+            children: [rect]
+        };
+
+        return XMOT.creation.element("group", opts);
+    }
+});
--- src/interaction/widgets/translatebox.js
+++ src/interaction/widgets/translatebox.js
@@ -11,6 +11,8 @@ XMOT.namespace("XMOT.interaction.widgets");
 XMOT.interaction.widgets.TranslateBox = new XMOT.Class(
     XMOT.interaction.widgets.Widget, {
 
+    GeoConstructorType: XMOT.interaction.geometry.TranslateBoxGeoConstructor,
+
     listenerTypes: [
        "dragstart", "dragend", "touch"// args: (this, sensor, original event)
     ],
@@ -52,53 +54,6 @@ XMOT.interaction.widgets.TranslateBox = new XMOT.Class(
     // --------------------------------
     // -- Creation --
     // --------------------------------
-
-    /**
-     *  @this {XMOT.interaction.widgets.TranslateBox}
-     *  @override
-     *  @protected
-     */
-    onCreateDefsElements: function()
-    {
-        // shaders
-        this.geo.addShaders("s_transl", {
-            diffCol: "1 1 1", transp: "0.85"
-        });
-        this.geo.addShaders("s_transl_highlight", {
-            diffCol: "1 1 1", transp: "0.4"
-        });
-
-        // transforms
-        this.geo.addTransforms("t_xytrans", {
-            translation: "0 0 1"});
-        this.geo.addTransforms("t_xytrans_inv", {
-            translation: "0 0 -1", rotation: "0 1 0 3.14"});
-        this.geo.addTransforms("t_yztrans", {
-            translation: "1 0 0", rotation: "0 1 0 1.57"});
-        this.geo.addTransforms("t_yztrans_inv", {
-            translation: "-1 0 0", rotation: "0 1 0 -1.57"});
-        this.geo.addTransforms("t_xztrans", {
-            translation: "0 1 0", rotation: "1 0 0 -1.57"});
-        this.geo.addTransforms("t_xztrans_inv", {
-            translation: "0 -1 0", rotation: "1 0 0 1.57"});
-    },
-
-    /**
-     *  @this {XMOT.interaction.widgets.TranslateBox}
-     *  @override
-     *  @protected
-     */
-    onCreateGraph: function()
-    {
-        this.geo.addToGraphRoot([
-             this._createRectGrp("xytrans"),
-             this._createRectGrp("yztrans"),
-             this._createRectGrp("xztrans"),
-             this._createRectGrp("xytrans_inv"),
-             this._createRectGrp("yztrans_inv"),
-             this._createRectGrp("xztrans_inv")
-        ]);
-    },
 
     /**
      *  @this {XMOT.interaction.widgets.TranslateBox}
@@ -189,25 +144,5 @@ XMOT.interaction.widgets.TranslateBox = new XMOT.Class(
         this.behavior[localID].addListener("dragstart", function(sensor, ev){self._onDragStart(sensor, ev);});
         this.behavior[localID].addListener("dragend", function(sensor, ev){self._onDragEnd(sensor, ev);});
         this.behavior[localID].addListener("touch", this.callback("_onTouch"));
-    },
-
-    /**
-     *  @this {XMOT.interaction.widgets.TranslateBox}
-     *  @private
-     *
-     *  @param {string} localID
-     */
-    _createRectGrp: function(localID)
-    {
-        var rect = XMOT.creation.rectangle(this.xml3d);
-
-        var opts = {
-            id: this.globalID(localID),
-            transform: "#" + this.globalID("t_" + localID),
-            shader: "#" + this.globalID("s_transl"),
-            children: [rect]
-        };
-
-        return XMOT.creation.element("group", opts);
     }
 });
