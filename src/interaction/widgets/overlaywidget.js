e0dd9ef62b079c8e01975f5abc0ab73b035f40d2
Boris Broenner <borisbroenner@gmail.com>
2013-04-01 17:45:11 +0200

    interaction-overlaywidget: added method to create a reflecting constraint
    It reflects the new pose properties to the widget's root element

--- src/interaction/widgets/overlaywidget.js
+++ src/interaction/widgets/overlaywidget.js
@@ -41,5 +41,53 @@ XMOT.interaction.widgets.OverlayWidget = new XMOT.Class(
     mirror: function()
     {
         return this._mirror;
+    },
+
+    /** In addition to asking given constraint functions whether the constraint should apply
+     *  it also set's the target node's properties, if the constraint admits the new values.
+     *
+     *  @param {Object} the constraint functions to be applied. They have the same signature
+     *      and name as in XMOT.Constraint.
+     */
+    createReflectingConstraint: function(options)
+    {
+        // setup the target node: it is the real target node's parent node
+        var realTargetParent = this._mirror.target().object.parentNode;
+        if(!realTargetParent)
+            throw new Error("XMOT.interaction.widgets.OverlayWidget: target doesn't have a parent node.");
+
+        var target = XMOT.ClientMotionFactory.createTransformable(
+            this._mirror.target().object.parentNode);
+
+        var options = options || {};
+
+        return {
+            constrainRotation: function(newRotation, opts){
+
+                if(options.constrainRotation && !options.constrainRotation(newRotation, opts))
+                    return false;
+
+                target.setOrientation(newRotation);
+                return true;
+            },
+
+            constrainScaling: function(newScale, opts){
+
+                if(options.constrainScaling && !options.constrainScaling(newScale, opts))
+                    return false;
+
+                target.setScale(newScale);
+                return true;
+            },
+
+            constrainTranslation: function(newTranslation, opts) {
+
+                if(options.constrainTranslation && !options.constrainTranslation(newTranslation, opts))
+                    return false;
+
+                target.setPosition(newTranslation);
+                return true;
+            }
+        };
     }
 });
