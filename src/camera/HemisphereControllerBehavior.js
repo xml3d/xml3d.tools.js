30dce96f1f21994950658e1838f786b34e4500b8
Boris Broenner <bobbel@cg.uni-saarland.de>
2013-04-15 13:38:00 +0200

    camera: added new version of hemisphere controller

---
+++ src/camera/HemisphereControllerBehavior.js
@@ -0,0 +1,96 @@
+(function(){
+
+    "use strict";
+
+    /** This behavior will provide "examine" mode camera control on a hemisphere.
+     *  From outside the methods doDollyAction() and doRotateAction() should be called.
+     *  This behavior does have no notion of the interaction device. All it needs
+     *  are deltaX and deltaY values, from which it computes the camera pose.
+     *
+     *  @constructor
+     */
+    XMOT.HemisphereControllerBehavior = new XMOT.Class(
+        XMOT.ExamineControllerBehavior, {
+
+        initialize: function(targetViewTransformable, options) {
+
+            this.callSuper(targetViewTransformable, options);
+
+            this._latitude = 0;
+            this._longitude =  0;
+            this._radius = 1;
+
+            this.lookAt(this.getExamineOrigin());
+        },
+
+        /**
+         *  @this {XMOT.HemisphereControllerBehavior}
+         *  @override
+         */
+        doDollyAction: function(deltaX, deltaY) {
+
+            this.callSuper(deltaX, deltaY);
+            this._radius = this.target.getPosition().length();
+        },
+
+        /**
+         *  @this {XMOT.HemisphereControllerBehavior}
+         *  @override
+         */
+        doRotateAction: function(deltaX, deltaY) {
+
+            this._longitude += deltaX * Math.PI / 2.0;
+            this._latitude += deltaY * Math.PI / 4.0;
+            this._latitude = Math.max(0, Math.min(Math.PI / 2.0, this._latitude));
+
+            var cos_latitude = Math.cos(this._latitude);
+            var cos_longitude = Math.cos(this._longitude);
+            var sin_longitude = Math.sin(this._longitude);
+
+            // Position
+            var position = new window.XML3DVec3(cos_latitude * sin_longitude, Math.sin(this._latitude),
+                    cos_latitude * cos_longitude);
+            position = position.normalize();
+            position = position.scale(this._radius);
+
+            // Right
+            var right = new window.XML3DVec3(cos_longitude,0,-sin_longitude);
+            right = right.normalize();
+
+            // direction
+            var direction = (new window.XML3DVec3(0,0,0)).subtract(position);
+            direction = direction.normalize();
+
+            // up
+            var up = right.cross(direction);
+
+            var orientation = new window.XML3DRotation();
+            orientation.setFromBasis(right, up, direction.negate());
+
+            this.target.setPosition(position);
+            this.target.setOrientation(orientation);
+        },
+
+        lookAt: function(targetPt) {
+            this._setViewDirection(targetPt.subtract(this.target.getPosition()));
+        },
+
+        _setViewDirection: function(dir) {
+            if (dir.length() < 1E-10)
+                return;
+
+            var yAxis = this.target.getOrientation().rotateVec3(new window.XML3DVec3(0,1,0));
+
+            var xAxis = dir.cross(yAxis);
+            if (xAxis.length() < 1E-10)
+            {
+                xAxis = this.target.getOrientation().rotateVec3(new window.XML3DVec3(1,0,0));
+            }
+
+            var orientation = new window.XML3DRotation();
+            orientation.setFromBasis(xAxis, xAxis.cross(dir), dir.negate());
+
+            this.target.setOrientation(orientation);
+        }
+    });
+}());
---
+++ src/camera/MouseHemisphereController.js
@@ -0,0 +1,61 @@
+(function(){
+
+    "use strict";
+
+    /** This controller brings together the mouse control and XMOT.HemisphereControllerBehavior
+     *  to provide examine mode navigation using the mouse.
+     *
+     *  @constructor
+     */
+    XMOT.MouseHemisphereController = new XMOT.Class(XMOT.MouseController, {
+
+        // interaction types
+        NONE: 0,
+        ROTATE: 1,
+        DOLLY: 2,
+
+        initialize: function(targetViewTransformable, options) {
+
+            this.callSuper(targetViewTransformable, options);
+
+            this._controller = new XMOT.HemisphereControllerBehavior(targetViewTransformable, options);
+            this._currentAction = this.NONE;
+        },
+
+        /**
+         *  @this {XMOT.ExamineController}
+         *  @protected
+         */
+        doActivate: function(action) {
+
+            this._currentAction = this.ROTATE;
+            if(action.evt.button === XMOT.MOUSEBUTTON_RIGHT)
+                this._currentAction = this.DOLLY;
+        },
+
+        /**
+         *  @this {XMOT.ExamineController}
+         *  @protected
+         */
+        doAction: function(action) {
+
+            switch (this._currentAction) {
+            case this.DOLLY:
+                this._controller.doDollyAction(action.delta.x, action.delta.y);
+                break;
+
+            case this.ROTATE:
+                this._controller.doRotateAction(action.delta.x, action.delta.y);
+                break;
+            }
+        },
+
+        /**
+         *  @this {XMOT.ExamineController}
+         *  @protected
+         */
+        doDeactivate: function(action) {
+            this._currentAction = this.NONE;
+        }
+    });
+}());
