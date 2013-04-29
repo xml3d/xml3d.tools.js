933048187f4da68068bd12b5d067b44f0b4bb31b
Boris Broenner <bobbel@cg.uni-saarland.de>
2013-04-29 16:48:29 +0200

    examples-viewtracker: fixed test

--- examples/viewtracker.js
+++ examples/viewtracker.js
@@ -1,20 +1,17 @@
 
 var xml3d = null;
-var headLight = null;
 var tracker = null;
+var camCtrl = null;
 
 window.addEventListener('load', initScene, false);
 
 function initScene()
 {
-    // setup initial variables
     xml3d = $("xml3d")[0];
-    // setup view
-    //$("#my_view")[0].lookAt(new XML3DVec3(0, 0, 0));
 
-    var ctrl = XML3D.Xml3dSceneController.getController(xml3d);
-    ctrl.mode = "lookaround";
-    ctrl.zoomSpeed = 1;
+	var camTransformable = XMOT.ClientMotionFactory.createTransformable($("#g_myview")[0]);
+    camCtrl = new XMOT.MouseKeyboardFlyController(camTransformable);
+	camCtrl.attach();
 
     // setup tracker
     tracker = new XMOT.ViewTracker(xml3d, onViewXfmChanged);
