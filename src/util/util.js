e546bd78bcc6a76ec544413f11c0beec02584265
Boris Broenner <bobbel@cg.uni-saarland.de>
2013-04-30 12:50:37 +0200

    util: added method isTransformable() and getOrCreateTransformable()

--- src/util/util.js
+++ src/util/util.js
@@ -365,4 +365,38 @@
 
         xml3d.removeEventListener("framedrawn", onFrameDrawn, false);
     };
+
+	function isFunction(object)
+	{
+		return object !== undefined && typeof object === "function";
+	};
+
+	/** Returns whether the given object is a Transformable
+	 *
+	 * 	@param {Object} object
+	 * 	@returns {boolean} true if the given object represents a Transformable
+	 */
+	u.isTransformable = function(object)
+	{
+		if(!object)
+			return false;
+
+		return isFunction(object.setPosition) && isFunction(object.setOrientation) &&
+			isFunction(object.setScale) && isFunction(object.getPosition) &&
+			isFunction(object.getOrientation) && isFunction(object.getScale);
+	};
+
+	/** Returns the given object itself, if it is a Transformable, else
+	 *  constructs a default Transformable.
+	 *
+	 * 	@param {Element|Transformable} object
+	 * 	@returns {Transformable}
+	 */
+	u.getOrCreateTransformable = function(object)
+	{
+		if(u.isTransformable(object))
+			return object;
+
+		return XMOT.ClientMotionFactory.createTransformable(object);
+	};
 }());
