91bfcb0d6822c57e1de3042caa6606dec6a3ecf3
Boris Broenner <borisbroenner@gmail.com>
2013-05-23 12:13:34 +0200

    util.js: moved transformable helpers to own file

--- src/util/util.js
+++ src/util/util.js
@@ -111,40 +111,6 @@
 
         return bbox;
     };
-
-
-
-	function isFunction(object)
-	{
-		return object !== undefined && typeof object === "function";
-	};
-
-	/** Returns whether the given object is a Transformable
-	 *
-	 * 	@param {Object} object
-	 * 	@returns {boolean} true if the given object represents a Transformable
-	 */
-	u.isTransformable = function(object)
-	{
-		if(!object)
-			return false;
-
-		return isFunction(object.setPosition) && isFunction(object.setOrientation) &&
-			isFunction(object.setScale) && isFunction(object.getPosition) &&
-			isFunction(object.getOrientation) && isFunction(object.getScale);
-	};
-
-	/** Returns the given object itself, if it is a Transformable, else
-	 *  constructs a default Transformable.
-	 *
-	 * 	@param {Element|Transformable} object
-	 * 	@returns {Transformable}
-	 */
-	u.getOrCreateTransformable = function(object)
-	{
-		if(u.isTransformable(object))
-			return object;
-
-		return XMOT.ClientMotionFactory.createTransformable(object);
-	};
 }());
+
+
