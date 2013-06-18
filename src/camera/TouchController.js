52399b1822cf6fd34fe4d4d28d7e4a703f382a19
Boris Broenner <borisbroenner@gmail.com>
2013-06-18 11:19:01 +0200

    camera-touchctrl: renamed callbacks

--- src/camera/TouchController.js
+++ src/camera/TouchController.js
@@ -113,9 +113,9 @@
             }
 
             regFn(this._targetXml3d, "touchstart", this.callback("_onXML3DTouchStart"));
-            regFn(document, "touchmove", this.callback("_onXML3DTouchMove"));
-            regFn(document, "touchend", this.callback("_onXML3DTouchEnd"));
-            regFn(document, "touchcancel", this.callback("_onXML3DTouchEnd"));
+            regFn(document, "touchmove", this.callback("_onDocumentTouchMove"));
+            regFn(document, "touchend", this.callback("_onDocumentTouchEnd"));
+            regFn(document, "touchcancel", this.callback("_onDocumentTouchEnd"));
         },
 
         // --- Callbacks ---
@@ -136,7 +136,7 @@
          *  @this {XMOT.TouchController}
          *  @private
          */
-        _onXML3DTouchMove: function(evt) {
+        _onDocumentTouchMove: function(evt) {
             if (evt.target.nodeName.toLowerCase() == "xml3d") {
                 evt.preventDefault();
             }
@@ -153,7 +153,7 @@
          *  @this {XMOT.TouchController}
          *  @private
          */
-        _onXML3DTouchEnd: function(evt) {
+        _onDocumentTouchEnd: function(evt) {
             if (evt.target.nodeName.toLowerCase() == "xml3d") {
                 evt.preventDefault();
             }
