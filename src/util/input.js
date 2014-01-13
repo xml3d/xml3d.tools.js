2570993d878bbbc0512d6eb115fc108b6bf9cd6c
Boris Broenner <borisbroenner@gmail.com>
2014-01-13 08:53:36 +0100

    util-input: added more keys

--- src/util/input.js
+++ src/util/input.js
@@ -8,7 +8,40 @@
     XML3D.tools.MOUSEBUTTON_RIGHT = 2;
 
     // --- keys ---
-	XML3D.tools.KEY_A = 65;
+    XML3D.tools.KEY_BACKSPACE = 8;
+    XML3D.tools.KEY_TAB = 9;
+    XML3D.tools.KEY_ENTER = 13;
+    XML3D.tools.KEY_SHIFT = 16;
+    XML3D.tools.KEY_CTRL = 17;
+    XML3D.tools.KEY_ALT = 18;
+    XML3D.tools.KEY_PAUSE = 19;
+    XML3D.tools.KEY_CAPSLOCK = 20;
+    XML3D.tools.KEY_ESCAPE = 27;
+    XML3D.tools.KEY_PAGEUP = 33;
+    XML3D.tools.KEY_PAGEDOWN = 34;
+    XML3D.tools.KEY_END = 35;
+    XML3D.tools.KEY_HOME = 36;
+
+    XML3D.tools.KEY_LEFT = 37;
+    XML3D.tools.KEY_UP = 38;
+    XML3D.tools.KEY_RIGHT = 39;
+    XML3D.tools.KEY_DOWN = 40;
+
+    XML3D.tools.KEY_INSERT = 45;
+    XML3D.tools.KEY_DELETE = 46;
+
+    XML3D.tools.KEY_0 = 48;
+    XML3D.tools.KEY_1 = 49;
+    XML3D.tools.KEY_2 = 50;
+    XML3D.tools.KEY_3 = 51;
+    XML3D.tools.KEY_4 = 52;
+    XML3D.tools.KEY_5 = 53;
+    XML3D.tools.KEY_6 = 54;
+    XML3D.tools.KEY_7 = 55;
+    XML3D.tools.KEY_8 = 56;
+    XML3D.tools.KEY_9 = 57;
+
+    XML3D.tools.KEY_A = 65;
     XML3D.tools.KEY_B = 66;
     XML3D.tools.KEY_C = 67;
     XML3D.tools.KEY_D = 68;
@@ -35,12 +68,51 @@
     XML3D.tools.KEY_Y = 89;
     XML3D.tools.KEY_Z = 90;
 
-    XML3D.tools.KEY_PGUP = 33;
-    XML3D.tools.KEY_PGDOWN = 34;
+    XML3D.tools.KEY_WINLEFT = 91;
+    XML3D.tools.KEY_WINRIGHT = 92;
+    XML3D.tools.KEY_SELECT = 93;
 
-    // arrow keys
-    XML3D.tools.KEY_LEFT = 37;
-    XML3D.tools.KEY_UP = 38;
-    XML3D.tools.KEY_RIGHT = 39;
-    XML3D.tools.KEY_DOWN = 40;
+    XML3D.tools.KEY_NUMPAD0 = 96;
+    XML3D.tools.KEY_NUMPAD1 = 97;
+    XML3D.tools.KEY_NUMPAD2 = 98;
+    XML3D.tools.KEY_NUMPAD3 = 99;
+    XML3D.tools.KEY_NUMPAD4 = 100;
+    XML3D.tools.KEY_NUMPAD5 = 101;
+    XML3D.tools.KEY_NUMPAD6 = 102;
+    XML3D.tools.KEY_NUMPAD7 = 103;
+    XML3D.tools.KEY_NUMPAD8 = 104;
+    XML3D.tools.KEY_NUMPAD9 = 105;
+
+    XML3D.tools.KEY_NUMPAD_MULTIPLY = 106;
+    XML3D.tools.KEY_NUMPAD_ADD = 107;
+    XML3D.tools.KEY_NUMPAD_SUBTRACT = 109;
+    XML3D.tools.KEY_NUMPAD_DECIMAL = 110;
+    XML3D.tools.KEY_NUMPAD_DIVIDE = 111;
+
+    XML3D.tools.KEY_F1 = 112;
+    XML3D.tools.KEY_F2 = 113;
+    XML3D.tools.KEY_F3 = 114;
+    XML3D.tools.KEY_F4 = 115;
+    XML3D.tools.KEY_F5 = 116;
+    XML3D.tools.KEY_F6 = 117;
+    XML3D.tools.KEY_F7 = 118;
+    XML3D.tools.KEY_F8 = 119;
+    XML3D.tools.KEY_F9 = 120;
+    XML3D.tools.KEY_F10 = 121;
+    XML3D.tools.KEY_F11 = 122;
+    XML3D.tools.KEY_F12 = 123;
+
+    XML3D.tools.KEY_NUMLOCK = 144;
+    XML3D.tools.KEY_SCROLLLOCK = 145;
+    XML3D.tools.KEY_SEMICOLON = 186;
+    XML3D.tools.KEY_EQUALS = 187;
+    XML3D.tools.KEY_COMMA = 188;
+    XML3D.tools.KEY_DASH = 189;
+    XML3D.tools.KEY_PERIOD = 190;
+    XML3D.tools.KEY_SLASH = 191;
+    XML3D.tools.KEY_GRAVE_ACCENT = 192;
+    XML3D.tools.KEY_BRACKET_OPEN = 219;
+    XML3D.tools.KEY_BACKSLASH = 220;
+    XML3D.tools.KEY_BRACKET_CLOSE = 221;
+    XML3D.tools.KEY_SINGLE_QUOTE = 222;
 }());
