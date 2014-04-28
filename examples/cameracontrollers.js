1a352706f4544aea323cefb1bba00a599b0dfd70
Boris Broenner <borisbroenner@gmail.com>
2014-04-28 15:52:48 +0200

    examples-camctrl: added test for custom controls

--- examples/cameracontrollers.js
+++ examples/cameracontrollers.js
@@ -4,19 +4,20 @@ var camCtrl = null;
 var camRadios = null;
 var camTransformable = null;
 
-function onLoad(){
-
+function onLoad()
+{
     camRadios = document.getElementsByName('camtype');
     $(camRadios).click(onChangeCamType);
 
     camTransformable = XML3D.tools.MotionFactory.createTransformable($("#g_camera")[0]);
 
     onChangeCamType();
-};
+}
 
-function onChangeCamType() {
-
-    if(camCtrl) {
+function onChangeCamType()
+{
+    if (camCtrl)
+    {
         camCtrl.detach();
         camCtrl = null;
     }
@@ -24,7 +25,7 @@ function onChangeCamType() {
     var CamType = null;
     var options = {};
 
-    switch(getCamType())
+    switch (getCamType())
     {
     case "examine":
         CamType = XML3D.tools.ExamineController;
@@ -41,24 +42,50 @@ function onChangeCamType() {
         options.behavior = {rotateSpeed: 0.2};
         CamType = XML3D.tools.TouchFlyController;
         break;
-    }
 
-    if(!CamType)
+    case "customfly":
+        CamType = XML3D.tools.MouseKeyboardFlyController;
+        options = {
+            behavior: {
+                rotateSpeed: 0.5,
+                moveSpeed: 0.5
+            },
+            controls: {
+                rotationActivator: XML3D.tools.MOUSEBUTTON_RIGHT,
+                left: XML3D.tools.KEY_LEFT,
+                right: XML3D.tools.KEY_RIGHT,
+                forward: XML3D.tools.KEY_UP,
+                backward: XML3D.tools.KEY_DOWN
+            }
+        }
+        break;
+
+    default:
         return;
+    }
 
     camCtrl = new CamType(camTransformable, options);
     camCtrl.attach();
-};
+}
 
 function getCamType()
 {
     for (var i = 0; i < camRadios.length; i++)
     {
         if (camRadios[i].checked)
-        {
             return camRadios[i].value;
-        }
     }
 
     return "none";
-};
+}
+
+
+function createCustomFlyMouseEventDispatcher()
+{
+    var disp = new XML3D.tools.util.EventDispatcher();
+    disp.registerCustomHandler("mousedown", function(evt){
+        return evt.button === XML3D.tools.MOUSEBUTTON_RIGHT;
+    });
+
+    return disp;
+}
