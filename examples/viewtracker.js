a2fd42dd76c99ea6dd77a81bca95371b894d2617
Boris Broenner <borisbroenner@gmail.com>
2014-05-06 16:04:20 +0200

    polished viewtracker

--- examples/viewtracker.js
+++ examples/viewtracker.js
@@ -22,10 +22,7 @@ function onViewXfmChanged()
 {
     var mat = XML3D.util.getOrCreateActiveView(xml3d).getWorldMatrix();
 
-    var transl = new window.XML3DVec3(mat.m41, mat.m42, mat.m43);
-    var rot = window.XML3DRotation.fromMatrix(mat);
-
     var target = $("#t_viewtrack")[0];
-    target.setAttribute("translation", transl.str());
-    target.setAttribute("rotation", rot.str());
+    target.translation.set(mat.translation());
+    target.rotation.set(mat.rotation());
 }
