d31e5b30099c3ad18ef96df560fb58b879fd4012
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-05-07 17:40:30 +0200

    Replace the version placeholder

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var concat = require('gulp-concat');
 var del = require('del');
 var uglify = require('gulp-uglify');
+var version = require("gulp-version-number");
 var pkg = require("./package.json");
 
 var srcFiles = [
@@ -12,17 +13,27 @@ var destination = "./build";
 
 gulp.task("default", function(){
 	var outputName = "xml3d.tools." + pkg.version + ".js";
+	var versionConfig = {
+		"value" : pkg.version + "-dev",
+		"replaces" : ["%VERSION%"]
+	};
 
 	gulp.src(srcFiles)
 			.pipe(concat(outputName))
+			.pipe(version(versionConfig))
 			.pipe(gulp.dest(destination));
 });
 
 gulp.task("release", function(){
 	var outputName = "xml3d.tools." + pkg.version + ".min.js";
+	var versionConfig = {
+		"value" : pkg.version,
+		"replaces" : ["%VERSION%"]
+	};
 
 	gulp.src(srcFiles)
 			.pipe(concat(outputName))
+			.pipe(version(versionConfig))
 			.pipe(uglify())
 			.pipe(gulp.dest(destination));
 });
